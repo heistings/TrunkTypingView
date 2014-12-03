056da3b0a588a42f64db5bf64008c99667e368f1
庞博 <bopang@sohu-inc.com>
2014-12-03 17:43:52 +0800

    init

---
+++ TrunkTypingView/TrunkTypingView.h
@@ -0,0 +1,13 @@
+//
+//  TrunkTypingView.h
+//  TrunkTypingView
+//
+//  Created by waterforest on 14-12-3.
+//  Copyright (c) 2014年 sohu-inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TrunkTypingView : NSObject
+
+@end
---
+++ TrunkTypingView/TrunkTypingView.m
@@ -0,0 +1,13 @@
+//
+//  TrunkTypingView.m
+//  TrunkTypingView
+//
+//  Created by waterforest on 14-12-3.
+//  Copyright (c) 2014年 sohu-inc. All rights reserved.
+//
+
+#import "TrunkTypingView.h"
+
+@implementation TrunkTypingView
+
+@end
