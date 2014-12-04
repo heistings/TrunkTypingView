b5dde7cc4234096bd3e19359ac89a4c6dc757cf2
庞博 <bopang@sohu-inc.com>
2014-12-04 14:47:29 +0800

    Interface designed.
    Signed-off-by: 庞博 <bopang@sohu-inc.com>

--- TrunkTypingView/TrunkTypingView.h
+++ TrunkTypingView/TrunkTypingView.h
@@ -6,8 +6,45 @@
 //  Copyright (c) 2014年 sohu-inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface TrunkTypingView : NSObject
+typedef NS_ENUM(NSInteger, TrunkTypingViewContentVerticalAlignment) {
+    TrunkTypingViewContentVerticalAlignmentCenter  = 0,
+    TrunkTypingViewContentVerticalAlignmentTop     = 1,
+    TrunkTypingViewContentVerticalAlignmentBottom  = 2,
+};
+
+typedef NS_ENUM(NSInteger, TrunkTypingViewFootType) {
+    TrunkTypingViewFootTypeLeftUp       = 0,
+    TrunkTypingViewFootTypeLeftDown     = 1,
+    TrunkTypingViewFootTypeRightUp      = 2,
+    TrunkTypingViewFootTypeRightDown    = 3
+};
+
+@protocol TrunkTypingViewDelegate;
+
+//______________________________________________________
+
+@interface TrunkTypingView : UIView
+
+@property (nonatomic, weak) id<TrunkTypingViewDelegate> delegate;
+
+@property (nonatomic) TrunkTypingViewFootType footType; // Default TrunkTypingViewFootTypeLeftDown
+@property (nonatomic) CGPoint foot; // Default CGPointZero
+
+@property (nonatomic) TrunkTypingViewContentVerticalAlignment leftButtonVerticalAlignment; // Default TrunkTypingViewContentVerticalAlignmentTop
+@property (nonatomic) TrunkTypingViewContentVerticalAlignment rightButtonVerticalAlignment; // Default TrunkTypingViewContentVerticalAlignmentTop
+@property (nonatomic, strong) UIButton *leftButton;
+@property (nonatomic, strong) UIButton *rightButton;
+
+@end
+
+//______________________________________________________
+
+
+@protocol TrunkTypingViewDelegate <NSObject>
+
+- (void)trunkTypingView:(TrunkTypingView *)view frameWillChange:(CGRect)frame;
+- (void)trunkTypingView:(TrunkTypingView *)view frameDidChange:(CGRect)frame;
 
 @end
